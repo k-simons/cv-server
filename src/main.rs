b28d2c69e2949fb044338fb1704251e9aa5b64f6
Kevin Simons <ksimons@palantir.com>
2020-03-22 12:35:35 -0700

    Can server some json

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+
+
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
 use serde::{Serialize, Deserialize};
+use rocket_contrib::json::{Json, JsonValue};
+
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/foo")]
+fn index_foo() -> &'static str {
+    "Hello, foo!"
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Point {
@@ -6,12 +23,15 @@ struct Point {
     y: i32,
 }
 
-fn main() {
+#[get("/barJson", format = "json")]
+fn index_bar() ->  Json<Point> {
     let point = Point { x: 1, y: 2 };
+    return Json(point)
+}
 
-    let serialized = serde_json::to_string(&point).unwrap();
-    println!("serialized = {}", serialized);
 
-    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
-    println!("deserialized = {:?}", deserialized);
+fn main() {
+    println!("Mount!");
+    rocket::ignite().mount("/", routes![index, index_foo, index_bar]).launch();
+    println!("Mount over!");
 }
