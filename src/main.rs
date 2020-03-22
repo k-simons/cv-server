9365136c49726f4354192d36ea83640cd75fbadd
Kevin Simons <ksimons@palantir.com>
2020-03-22 12:50:13 -0700

    Can server some json

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@
 
 #[macro_use] extern crate rocket;
 use serde::{Serialize, Deserialize};
-use rocket_contrib::json::{Json, JsonValue};
+use rocket_contrib::json::{Json};
 
+
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -31,7 +36,23 @@ fn index_bar() ->  Json<Point> {
 
 
 fn main() {
+    println!("Start");
+    let u = read_user_from_file().unwrap();
+    println!("{:#?}", u);
     println!("Mount!");
     rocket::ignite().mount("/", routes![index, index_foo, index_bar]).launch();
     println!("Mount over!");
+}
+
+fn read_user_from_file() -> Result<Point, Box<Error>> {
+    // Open the file in read-only mode with buffer.
+    let file = File::open("/Users/ksimons/code/corona/cv-server/testPoint.json")?;
+    // let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    // Read the JSON contents of the file as an instance of `User`.
+    let u = serde_json::from_reader(reader)?;
+
+    // Return the `User`.
+    Ok(u)
 }
